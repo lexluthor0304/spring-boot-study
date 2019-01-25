2088beeceb8effeb00dfa8c51d87af1d29d05879
lexluthor0304 <lexluthor0304@gmail.com>
2019-01-25 14:44:03 +0900

    thymeleaf-layout-dialect
    step 1

---
+++ thymeleaf-layout-dialect/src/main/java/com/thymeleaf/layout/ThymeleafLayoutDialectApplication.java
@@ -0,0 +1,14 @@
+package com.thymeleaf.layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLayoutDialectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafLayoutDialectApplication.class, args);
+	}
+
+}
+
---
+++ thymeleaf-layout-dialect/src/test/java/com/thymeleaf/layout/ThymeleafLayoutDialectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.thymeleaf.layout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafLayoutDialectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
